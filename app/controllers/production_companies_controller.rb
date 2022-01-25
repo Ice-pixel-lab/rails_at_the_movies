403e5ce01fcd07e3a3c9b9aae3ccc6e0060f6b3a
Jesse <jsabel@academic.rrc.ca>
2022-01-25 16:33:33 -0600

    More HTML development

--- app/controllers/production_companies_controller.rb
+++ app/controllers/production_companies_controller.rb
@@ -1,5 +1,9 @@
 class ProductionCompaniesController < ApplicationController
-  def index; end
+  def index
+    @production_companies = ProductionCompany.all
+  end
 
-  def show; end
+  def show
+    @production_company = ProductionCompany.find(params[:id])
+  end
 end
