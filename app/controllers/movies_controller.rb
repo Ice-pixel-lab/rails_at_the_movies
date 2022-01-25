5ff6c6179d96c81c9d85e43080d219fe63c73cdf
Jesse <jsabel@academic.rrc.ca>
2022-01-25 15:25:53 -0600

    Used custom routing, modified movies index and show (action and view)

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,11 @@
 class MoviesController < ApplicationController
   def index
+    # @movies = Movie.order("average_vote DESC") # ORDER BY average_vote DESC --> SQL
+
+    @movies = Movie.includes(:production_company).all.order("average_vote DESC")
   end
 
   def show
+    @movie = Movie.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'production_companies/index'
-  get 'production_companies/show'
-  get 'movies/index'
-  get 'movies/show'
+  # get 'production_companies/index'
+  # get 'production_companies/show'
+  # get 'movies/index'
+  # get 'movies/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # only: [:index, :show]
+  resources :movies, only: %i[index show]
+  resources :production_companies, only: %i[index show]
 
   # Defines the root path route ("/")
   # root "articles#index"
