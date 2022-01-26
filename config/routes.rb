5ccd11827014580a8f692d2c2c36a735555315a0
Jesse <jsabel@academic.rrc.ca>
2022-01-26 12:15:55 -0600

    Commit for Index

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  # get "home/index"
   # get 'production_companies/index'
   # get 'production_companies/show'
   # get 'movies/index'
   # get 'movies/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root to: "home#index"
   # only: [:index, :show]
   resources :movies, only: %i[index show]
   resources :production_companies, only: %i[index show]
